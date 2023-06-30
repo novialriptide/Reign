20581304c2f2d4cb6b58973ae00bc8872c91c03a
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-06-30 07:19:20 -0400

    Create DrawableGameObject.cs

---
+++ Faraway.Main/Engine/DrawableGameObject.cs
@@ -0,0 +1,11 @@
+﻿using Faraway.Main.Engine.Components;
+using System.Collections.Generic
+
+namespace Faraway.Main.Engine
+{
+    public abstract class DrawableGameObject : GameObject
+    {
+        public Transform Transform { get; private set; }
+        public Sprite2D Sprite2D { get; private set; }
+    }
+}
