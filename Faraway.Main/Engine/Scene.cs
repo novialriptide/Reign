21d8245ea159f5613a3d9500c937efe835ab2fb0
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-06-30 02:18:05 -0400

    Create Scene.cs

---
+++ Faraway.Main/Engine/Scene.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Faraway.Main.Engine
+{
+    public class Scene
+    {
+        public List<GameObject> GameObjects { get; }
+        public Scene()
+        {
+            GameObjects = new List<GameObject>();
+        }
+        /// <summary>
+        /// Add a <c>GameObject</c> to the <c>Scene</c>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        public T AddGameObject<T>(GameObject gameObject) where T: GameObject
+        {
+            GameObjects.Add(gameObject);
+
+            return (T)gameObject;
+        }
+        /// <summary>
+        /// Called when <c>Scene</c> is loaded.
+        /// </summary>
+        public virtual void OnStart() { }
+        /// <summary>
+        /// Called every frame; Should contain game logic.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Update(GameTime gameTime) { }
+        /// <summary>
+        /// Called every frame; should contain draw logic.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Draw(GameTime gameTime) { }
+        /// <summary>
+        /// Called when <c>Scene</c> is destroyed.
+        /// </summary>
+        public virtual void OnDestroy() { }
+    }
+}
