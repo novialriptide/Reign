7db42c0239a484c6f702169e3af6ec7b3b9ee25c
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-07 00:17:27 -0400

    Remove unused code

--- Faraway.Main/Engine/Components/SpriteRenderer.cs
+++ Faraway.Main/Engine/Components/SpriteRenderer.cs
@@ -6,21 +6,10 @@ namespace Faraway.Main.Engine.Components
     public class SpriteRenderer : Component
     {
         public Texture2D Texture;
-        private SpriteBatch spriteBatch;
         public SpriteRenderer() { }
         public SpriteRenderer(Texture2D texture)
         {
             Texture = texture;
-        }
-        public override void Update(float deltaTime)
-        {
-            Transform t = GameObject.GetComponent<Transform>();
-
-            spriteBatch.Draw(
-            Texture, t.Position, null, Color.White,
-                t.Rotation, t.RotationOrigin, t.Scale, SpriteEffects.None, 0f);
-
-            base.Update(deltaTime);
         }
     }
 }
