a8453f500175a7f34608aeb5718f081d1f61b010
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-19 09:15:31 -0400

    WIP: PlayerInteractionHandler

--- Faraway.Engine/Components/BoxCollider2D.cs
+++ Faraway.Engine/Components/BoxCollider2D.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Faraway.Engine.Components
+﻿namespace Faraway.Engine.Components
 {
     public class BoxCollider2D : Component
     {
+        // TODO: Implement
     }
 }
---
+++ Faraway.Main/Components/PlayerInteractionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Faraway.Engine.Components;
+
+namespace Faraway.Main.Components
+{
+    public class PlayerInteractionHandler : Component
+    {
+        public bool IsActive => !GameObject.Scene.IsPaused;
+        public List<SpaceCraft> GetSpaceCraftWithinRegion(Vector2 start, Vector2 end)
+        {
+            // ensure (x1,y1) and (x2,y2) will be topleft,bottomright
+            float x1 = Math.Min(start.X, end.X);
+            float y1 = Math.Min(start.Y, end.Y);
+            float x2 = Math.Max(start.X, end.X);
+            float y2 = Math.Max(start.Y, end.Y);
+
+            // TODO: Finish implementation
+
+            return new List<SpaceCraft>();
+        }
+    }
+}
