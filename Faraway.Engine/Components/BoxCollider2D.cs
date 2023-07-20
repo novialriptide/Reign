33ee2268433c66e1f12518a3eba5bb00f2e0959a
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-19 20:46:53 -0400

    Add CollidesWith

--- Faraway.Engine/Components/BoxCollider2D.cs
+++ Faraway.Engine/Components/BoxCollider2D.cs
@@ -1,7 +1,17 @@
-﻿namespace Faraway.Engine.Components
+﻿using System.Numerics;
+
+namespace Faraway.Engine.Components
 {
     public class BoxCollider2D : Component
     {
-        // TODO: Implement
+        public Vector2 Offset;
+        public Vector2 Size;
+        public bool CollidesWith(BoxCollider2D collider)
+        {
+            return Offset.X + Size.X >= collider.Offset.X &&
+                Offset.X <= collider.Offset.X + collider.Size.X &&
+                Offset.Y + Size.Y >= collider.Offset.Y &&
+                Offset.Y <= collider.Offset.Y + collider.Size.Y;
+        }
     }
 }
