9f43aafc84de0e0de2faa7bf505c132b3339df67
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-23 00:37:38 -0400

    Working button presses

--- Faraway.Engine/Input/MouseInput.cs
+++ Faraway.Engine/Input/MouseInput.cs
@@ -1,94 +1,36 @@
-﻿using System.Diagnostics;
-using System.Numerics;
-using System.Runtime.Serialization;
+﻿using System.Numerics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Faraway.Engine.Input
 {
     public static class MouseInput
     {
-        private class MouseButton
-        {
-            /*
-             * TODO: `IsPressed` is if the mouse button is being held down or not.
-             * make an `IsClicked` as well.
-             */
-            private float timeSinceFirstPress = 0f;
-            private bool isTimerActive = false;
-            public bool IsPressed = false;
-            public int ClickCount = 0;
-
-            public void Update(float deltaTime, ButtonState buttonState)
-            {
-                IsPressed = buttonState == ButtonState.Pressed;
-
-                if (IsPressed)
-                    isTimerActive = true;
-
-                if (isTimerActive)
-                    timeSinceFirstPress += deltaTime;
-
-                if (IsPressed && timeSinceFirstPress <= MouseDelay && isTimerActive)
-                {
-                    timeSinceFirstPress = 0;
-                    ClickCount += 1;
-                }
-                else if (timeSinceFirstPress > MouseDelay && isTimerActive)
-                {
-                    timeSinceFirstPress = 0;
-                    ClickCount = 0;
-                    isTimerActive = false;
-                }
-
-                Debug.WriteLine(ClickCount);
-            }
-        }
-
         private static MouseState mouseState;
 
         /// <summary>
-        /// Delay used to detect double click
+        /// Delay used to detect double click in milliseconds.
         /// </summary>
-        public static float MouseDelay = 0.5f;
+        public static int MouseDelay = 500;
         public static Vector2 MousePosition;
 
-        private static MouseButton leftButton = new MouseButton();
-        private static MouseButton middleButton = new MouseButton();
-        private static MouseButton rightButton = new MouseButton();
+        public static MouseButton LeftButton = new MouseButton();
+        public static MouseButton MiddleButton = new MouseButton();
+        public static MouseButton RightButton = new MouseButton();
 
-        public static bool IsLeftDown => leftButton.IsPressed;
-        public static bool IsRightDown => rightButton.IsPressed;
-        public static bool IsMiddleDown => middleButton.IsPressed;
-
-        /// <summary>
-        /// Amount of times the left button has been clicked within
-        /// `MouseInput.MouseDelay` seconds.
-        /// </summary>
-        public static int LeftClickCount => leftButton.ClickCount;
-        /// <summary>
-        /// Amount of times the middle button has been clicked within
-        /// `MouseInput.MouseDelay` seconds.
-        /// </summary>
-        public static int MiddleClickCount => middleButton.ClickCount;
-        /// <summary>
-        /// Amount of times the right button has been clicked within
-        /// `MouseInput.MouseDelay` seconds.
-        /// </summary>
-        public static int RightClickCount => rightButton.ClickCount;
         /// <summary>
         /// Stays 0 if no mouse scroll is detected.
         /// </summary>
         public static float MouseScrollDirection;
 
-        public static void Update(float deltaTime)
+        internal static void Update(float deltaTime)
         {
             mouseState = Mouse.GetState();
 
             MousePosition = new Vector2(mouseState.Position.X, mouseState.Position.Y);
 
-            leftButton.Update(deltaTime, mouseState.LeftButton);
-            middleButton.Update(deltaTime, mouseState.MiddleButton);
-            rightButton.Update(deltaTime, mouseState.RightButton);
+            LeftButton.Update(deltaTime, mouseState.LeftButton, MouseDelay);
+            MiddleButton.Update(deltaTime, mouseState.MiddleButton, MouseDelay);
+            RightButton.Update(deltaTime, mouseState.RightButton, MouseDelay);
         }
     }
 }
