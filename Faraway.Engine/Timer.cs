cdea5bb25ea6b8d78c9040e75fac0c2d28eb6875
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-23 00:39:31 -0400

    Create Timer.cs

---
+++ Faraway.Engine/Timer.cs
@@ -0,0 +1,22 @@
+﻿namespace Faraway.Engine
+{
+    /// <summary>
+    /// Uses `deltaTime` to see how much time has passed. This is used to prevent the
+    /// game from proceeding with the times if there was a huge lagspike.
+    /// </summary>
+    public class Timer
+    {
+        public bool IsRunning = false;
+        public double Time = 0f;
+
+        public void Reset()
+        {
+            Time = 0;
+        }
+        public void Update(float deltaTime)
+        {
+            if (IsRunning)
+                Time += deltaTime;
+        }
+    }
+}
