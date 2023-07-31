a286f422fefdcbe142fc6fba4fc0778cf8ad8601
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-07-31 03:44:14 -0400

    Create RigidBody2D.cs

---
+++ Faraway.Engine/Components/RigidBody2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faraway.Engine.Components
+{
+    public class RigidBody2D : Component
+    {
+        private Transform transform;
+        public Vector2 Velocity = Vector2.Zero;
+        public Vector2 Acceleration = Vector2.Zero;
+        public override void Start()
+        {
+            transform = GameObject.GetComponent<Transform>();
+            base.Start();
+        }
+        public override void Update(double deltaTime)
+        {
+            transform.Position += Velocity * (float)deltaTime;
+            Velocity += Acceleration;
+            base.Update(deltaTime);
+        }
+    }
+}
