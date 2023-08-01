03fc5108e25af8fbed47035da20db07da8c35a70
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-01 03:33:34 -0400

    Add more testcases

--- Faraway.Engine.Tests/TestMath/TestVector2.cs
+++ Faraway.Engine.Tests/TestMath/TestVector2.cs
@@ -17,5 +17,25 @@ namespace Faraway.Engine.Tests.TestMath
             var a = new Vector2(756.2f, 24.6f);
             Assert.AreEqual(Math.Magnitude(a), 756.6000366210938);
         }
+        [TestMethod]
+        public void TestSetMagnitudeInteger()
+        {
+            var a = new Vector2(5, 5);
+            a = Math.SetMagnitude(a, 1);
+            Assert.AreEqual(Math.Magnitude(a), 1);
+
+            a = Math.SetMagnitude(a, 765);
+            Assert.AreEqual(Math.Magnitude(a), 765);
+        }
+        [TestMethod]
+        public void TestSetMagnitudeFloat()
+        {
+            var a = new Vector2(2346754.43f, 346.24f);
+            a = Math.SetMagnitude(a, 1);
+            Assert.AreEqual(Math.Magnitude(a), 1);
+
+            a = Math.SetMagnitude(a, 4.25f);
+            Assert.AreEqual(Math.Magnitude(a), 4.25f);
+        }
     }
 }
