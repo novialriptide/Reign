010f30cb24f6ab99519a0fbe4a486aacb4a5cc4a
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-01 03:33:40 -0400

    SetMagnitude

--- Faraway.Engine/Math.cs
+++ Faraway.Engine/Math.cs
@@ -5,13 +5,27 @@ namespace Faraway.Engine
 {
     public static class Math
     {
+        /// <summary>
+        /// Gets the magnitude.
+        /// </summary>
         public static float Magnitude(Vector2 vector2) => MathF.Sqrt(vector2.X * vector2.X + vector2.Y * vector2.Y);
+        /// <summary>
+        /// Sets the magnitude of the specified vector2.
+        /// </summary>
+        public static Vector2 SetMagnitude(Vector2 vector2, float magnitude)
+        {
+            // Referenced from: https://stackoverflow.com/questions/41317291/setting-the-magnitude-of-a-2d-vector
+            float currentMagnitude = Magnitude(vector2);
+            float ratio = magnitude / currentMagnitude;
+            return vector2 * ratio;
+        }
         /// <summary>
         /// Moves a Vector2 toward the target.
         /// </summary>
         /// <returns>The updated position.</returns>
         public static Vector2 MoveTowards(Vector2 current, Vector2 target, float maxDistanceDelta)
         {
+            // Referenced from: https://github.com/pygame/pygame/pull/2929
             Vector2 a = target - current;
             float magnitude = Magnitude(a);
             if (magnitude <= maxDistanceDelta || magnitude == 0f)
