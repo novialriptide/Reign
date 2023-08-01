e6d552d1811b267b407201e5772f677d4d0accc0
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-01 01:36:59 -0400

    Add `Faraway.Engine.Tests` project

---
+++ Faraway.Engine.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Faraway.Engine;
+
+namespace Faraway.Engine.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
