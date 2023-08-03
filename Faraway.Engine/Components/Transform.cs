55252c945fa2886a78ac5df9316f92c2d9c3905b
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-03 08:21:23 -0400

    API changes

--- Faraway.Engine/Components/Transform.cs
+++ Faraway.Engine/Components/Transform.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Faraway.Engine.Components
 {
@@ -24,14 +21,13 @@ namespace Faraway.Engine.Components
         /// </summary>
         public Vector2 Scale = new Vector2(1f, 1f);
         /// <summary>
-        /// Rotation of the Component in Radians.
+        /// Rotation of the Component in rotations.
         /// </summary>
         public float Rotation = 0f;
         /// <summary>
         /// Rotation Anchor of the Component.
         /// </summary>
         public Vector2 RotationOrigin = Vector2.Zero;
-
         private Transform parent;
         /// <summary>
         /// Parent of this GameObject.
@@ -47,7 +43,7 @@ namespace Faraway.Engine.Components
                     return;
                 }
 
-                if (Parent.GetChildren().Contains(value))
+                if (Parent.Children.Contains(value))
                     throw new ArgumentException("Cannot make circular reference of parents");
 
                 if (value.Parent == value)
@@ -57,43 +53,50 @@ namespace Faraway.Engine.Components
             }
         }
         /// <summary>
-        /// 
+        /// Children of this GameObject.
         /// </summary>
-        /// <returns>Children of this <c>GameObject</c>.</returns>
-        public Transform[] GetChildren()
+        public Transform[] Children
         {
-            List<Transform> children = new List<Transform>();
-
-            foreach (GameObject go in GameObject.Scene.GameObjects)
+            get
             {
-                Transform t = go.GetComponent<Transform>();
-                if (t.Parent == this)
-                    children.Add(t);
-            }
+                List<Transform> children = new List<Transform>();
 
-            return children.ToArray();
+                foreach (GameObject go in GameObject.Scene.GameObjects)
+                {
+                    Transform t = go.GetComponent<Transform>();
+                    if (t.Parent == this)
+                        children.Add(t);
+                }
+
+                return children.ToArray();
+            }
         }
         /// <summary>
-        /// 
+        /// The position of the GameObject based on the world.
         /// </summary>
-        /// <returns>The position of the GameObject based on the world.</returns>
-        public Vector2 GetWorldPosition()
+        public Vector2 WorldPosition
         {
-            Vector2 value = Position;
-            Transform currentTransform = Parent;
-
-            while (currentTransform != null)
+            get
             {
-                value += currentTransform.Position;
-                currentTransform = currentTransform.Parent;
-            }
+                Vector2 value = Position;
+                Transform currentTransform = Parent;
 
-            return value;
+                while (currentTransform != null)
+                {
+                    value += currentTransform.Position;
+                    currentTransform = currentTransform.Parent;
+                }
+
+                return value;
+            }
         }
-        public Vector2 GetScreenPosition()
+        public Vector2 ScreenPosition
         {
-            Transform camera = GameObject.Scene.ActiveCamera.GameObject.GetComponent<Transform>();
-            return camera.Position + GetWorldPosition();
+            get
+            {
+                Transform camera = GameObject.Scene.ActiveCamera.GameObject.GetComponent<Transform>();
+                return camera.Position + WorldPosition;
+            }
         }
         /// <summary>
         /// Not implemented.
@@ -103,5 +106,8 @@ namespace Faraway.Engine.Components
         {
             return Vector2.Zero;
         }
+
+        [Obsolete("Use property `WorldPosition` instead.")]
+        public Vector2 GetWorldPosition() => WorldPosition;
     }
 }
