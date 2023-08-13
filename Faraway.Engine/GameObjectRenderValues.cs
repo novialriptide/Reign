c375be037ac5cfd6b90fc89d19d9704ded5b5be8
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-13 00:54:18 -0400

    Move internal class into own file

---
+++ Faraway.Engine/GameObjectRenderValues.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using Faraway.Engine.Components;
+
+namespace Faraway.Engine
+{
+    internal class GameObjectRenderValues
+    {
+        internal readonly Vector2 Position = Vector2.Zero;
+        internal readonly float Rotation = 0f;
+        internal readonly Vector2 RotationOrigin = Vector2.Zero;
+        internal readonly Vector2 Scale = Vector2.One;
+
+        internal GameObjectRenderValues(GameObject gameObject)
+        {
+            Transform currentTransform = gameObject.GetComponent<Transform>();
+            while (currentTransform is not null)
+            {
+                Rotation += currentTransform.Rotation;
+
+                if (currentTransform.IsChild)
+                    RotationOrigin -= currentTransform.Position + currentTransform.RotationOrigin;
+
+                Scale *= currentTransform.Scale;
+
+                // Next loop
+                currentTransform = currentTransform.Parent;
+
+                /*
+                 * Changing the rotation origin changes the render position, therefore we
+                 * ignore the first position.
+                 */
+                if (currentTransform is not null)
+                    Position += currentTransform.Position;
+            }
+        }
+    }
+}
--- Faraway.Engine/Scene.cs
+++ Faraway.Engine/Scene.cs
@@ -46,37 +46,6 @@ namespace Faraway.Engine
             for (int i = 0; i < GameObjects.Count; i++)
                 GameObjects[i].Update(gameTime.ElapsedGameTime.TotalSeconds);
         }
-        internal class GameObjectRenderValues
-        {
-            internal Vector2 Position = Vector2.Zero;
-            internal float Rotation = 0f;
-            internal Vector2 RotationOrigin = Vector2.Zero;
-            internal Vector2 Scale = Vector2.One;
-
-            internal GameObjectRenderValues(GameObject gameObject)
-            {
-                Transform currentTransform = gameObject.GetComponent<Transform>();
-                while (currentTransform is not null)
-                {
-                    Rotation += currentTransform.Rotation;
-
-                    if (currentTransform.IsChild)
-                        RotationOrigin -= currentTransform.Position + currentTransform.RotationOrigin;
-
-                    Scale *= currentTransform.Scale;
-
-                    // Next loop
-                    currentTransform = currentTransform.Parent;
-
-                    /*
-                     * Changing the rotation origin changes the render position, therefore we
-                     * ignore the first position.
-                     */
-                    if (currentTransform is not null)
-                        Position += currentTransform.Position;
-                }
-            }
-        }
 
         /// <summary>
         /// Called every frame; should contain draw logic.
