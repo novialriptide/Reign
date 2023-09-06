2fc14dd43ecdfee1a33280b14a85f02747a2e486
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-09-06 01:03:35 -0400

    Remove unused parentheses

--- Faraway.Engine/MathExtended/MathFl.cs
+++ Faraway.Engine/MathExtended/MathFl.cs
@@ -4,7 +4,7 @@ namespace Faraway.Engine.MathExtended
 {
     public static class MathFl
     {
-        public static float RadiansToDegrees(float radians) => (180 / (float)Math.PI) * radians;
-        public static float DegreesToRadians(float degrees) => ((float)Math.PI / 180) * degrees;
+        public static float RadiansToDegrees(float radians) => 180 / (float)Math.PI * radians;
+        public static float DegreesToRadians(float degrees) => (float)Math.PI / 180 * degrees;
     }
 }
