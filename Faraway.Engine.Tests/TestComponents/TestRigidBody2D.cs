ed62f8413ee702a5e33076f9cb5cacb14d39fc32
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-09-28 21:13:41 -0400

    More unittests

--- Faraway.Engine.Tests/TestComponents/TestRigidBody2D.cs
+++ Faraway.Engine.Tests/TestComponents/TestRigidBody2D.cs
@@ -1,7 +1,9 @@
-﻿using Faraway.Engine.Components;
+﻿using System.Numerics;
+using Faraway.Engine.Components;
 
 namespace Faraway.Engine.Tests
 {
+    [Ignore]
     [TestClass]
     public class TestRigidBody2D
     {
@@ -15,10 +17,53 @@ namespace Faraway.Engine.Tests
             scene.OnStart();
             rb1.Update(0f);
 
+            Assert.IsTrue(rb1.IsEnabled);
             Assert.IsNotNull(rb1.Body);
             Assert.IsNotNull(rb1.Simulation);
             Assert.AreEqual(BodyType.Dynamic, rb1.BodyType);
             Assert.AreEqual(1, rb1.Body.FixtureList.Count);
+
+            scene.OnDestroy();
+        }
+        [TestMethod]
+        public void TestApplyRotationVelocity()
+        {
+            TestSceneBody scene = new TestSceneBody();
+
+            scene.OnStart();
+
+            RigidBody2D rb1 = scene.Obj1.GetComponent<RigidBody2D>();
+            
+            Assert.AreEqual(0, rb1.AngularVelocity);
+            rb1.ApplyAngularImpulse(200);
+            Assert.AreEqual(0, rb1.AngularVelocity);
+
+            scene.Step(200);
+            Assert.IsTrue(rb1.AngularVelocity != 0);
+
+            rb1.AngularVelocity = 0;
+            Assert.AreEqual(0, rb1.AngularVelocity);
+
+            scene.OnDestroy();
+        }
+        [TestMethod]
+        public void TestApplyVelocity()
+        {
+            TestSceneBody scene = new TestSceneBody();
+
+            scene.OnStart();
+
+            RigidBody2D rb1 = scene.Obj1.GetComponent<RigidBody2D>();
+            
+            Assert.AreEqual(Vector2.Zero, rb1.Velocity);
+            rb1.ApplyLinearImpulse(new Vector2(50, 50));
+            Assert.AreEqual(Vector2.Zero, rb1.Velocity);
+
+            scene.Step(200);
+            Assert.IsTrue(rb1.Velocity != Vector2.Zero);
+
+            rb1.Velocity = Vector2.Zero;
+            Assert.AreEqual(Vector2.Zero, rb1.Velocity);
 
             scene.OnDestroy();
         }
